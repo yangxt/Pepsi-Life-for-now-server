67550d8a6520bd26f681aa81afe40ee0798288ac
Sebastien Villar <sebastien.villar@gmail.com>
2013-08-09 17:24:53 +0200

    Environment back to heroku

--- config/environments.rb
+++ config/environments.rb
@@ -17,14 +17,14 @@ require 'yaml'
 # require 'active_record'
 # require 'uri'
 
-# db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
+db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/mydb')
 
-# ActiveRecord::Base.establish_connection(
-#   :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-#   :host     => db.host,
-#   :port     => db.port,
-#   :username => db.user,
-#   :password => db.password,
-#   :database => db.path[1..-1],
-#   :encoding => 'utf8'
-# )
+ActiveRecord::Base.establish_connection(
+  :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+  :host     => db.host,
+  :port     => db.port,
+  :username => db.user,
+  :password => db.password,
+  :database => db.path[1..-1],
+  :encoding => 'utf8'
+)
