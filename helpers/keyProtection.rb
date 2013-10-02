d2451c1f2c789fe6aef9855a49a8aa2c6405cd29
Sebastien Villar <sebastien.villar@gmail.com>
2013-10-02 17:35:01 +0200

    New API key

--- helpers/keyProtection.rb
+++ helpers/keyProtection.rb
@@ -3,7 +3,7 @@ require './helpers/haltJsonp'
 module Sinatra
     module KeyProtection
 
-    API_KEY = "nd6YyykHsCygZZi64F"
+    API_KEY = "U6U2XoC4auaMm3fJoV"
     def keyProtected!
         if (!(api_key = params[:api_key]) || api_key != API_KEY)
             haltJsonp 403, "You must add a valid API key to every request"
